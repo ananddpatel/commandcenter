dd183361445586ef24d99fb3a46c868e1e430129
Anand Patel <anandpatel3@hotmail.com>
2020-05-08 22:37:33 -0400

    updated

--- index.js
+++ index.js
@@ -6,36 +6,36 @@ const { v4: uuidv4 } = require('uuid');
 const NodeSSH = require('node-ssh');
 const ssh = new NodeSSH();
 
-const password = '';
+const password = 'raspberry';
 
 var app = express();
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
-// viewed at http://localhost:8080
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.post('/exec', async function (req, res) {
+app.post('/exec', async function(req, res) {
   // exec('start cmd.exe');
   // res.send(['ok']);
-  await ssh.connect({
-    host: '',
-    username: '',
-    port: 22,
-    password,
-    tryKeyboard: true,
-    onKeyboardInteractive: (name, instructions, instructionsLang, prompts, finish) => {
-      if (prompts.length > 0 && prompts[0].prompt.toLowerCase().includes('password')) {
-        finish([password]);
-      }
-    },
-  });
-  const { stdout } = await ssh.execCommand(`sudo touch ${uuidv4()}`);
-  console.log('result', stdout);
+  // await ssh.connect({
+  //   host: '192.168.2.78',
+  //   username: 'pi',
+  //   port: 22,
+  //   password,
+  //   tryKeyboard: true,
+  //   onKeyboardInteractive: (name, instructions, instructionsLang, prompts, finish) => {
+  //     if (prompts.length > 0 && prompts[0].prompt.toLowerCase().includes('password')) {
+  //       finish([password]);
+  //     }
+  //   }
+  // });
+  // const { stdout } = await ssh.execCommand(`sudo touch ${uuidv4()}`);
+  // console.log('result', stdout);
 
-  res.send({ output: stdout });
+  // res.send({ output: stdout });
+  res.send({ output: 'ok' });
   // exec(`ssh -tt pi@192.168.2.78 "cd /opt/ && sudo touch ${uuidv4()}"`, (err, stdout, stderr) => {
   //   res.send({ output: stdout });
   // });
